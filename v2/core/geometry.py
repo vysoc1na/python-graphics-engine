a25605c7979669d853f68ab44c61fca5d76c46d7
vysoc1na <153538241+vysoc1na@users.noreply.github.com>
2023-12-28 16:31:21 +0100

    moved geometry to general constructor and added plane geometry + updated main example

--- v2/core/geometry.py
+++ v2/core/geometry.py
@@ -1,7 +1,7 @@
 import glm
 import numpy
 
-class BoxGeometry():
+class Geometry():
 	def __init__(
 		self,
 		size = (1, 1, 1),
@@ -16,6 +16,20 @@ class BoxGeometry():
 		self.scale = glm.vec3(scale)
 		# setup vertex data
 		self.setup_vertex_data()
+
+	def setup_vertex_data(self):
+		self.vertices = []
+		self.normals = []
+
+class BoxGeometry(Geometry):
+	def __init__(
+		self,
+		size = (1, 1, 1),
+		position = (0, 0, 0),
+		rotation = (0, 0, 0),
+		scale = (1, 1, 1)
+	):
+		super().__init__(size, position, rotation, scale)
 
 	def setup_vertex_data(self):
 		self.vertices = numpy.array([
@@ -56,7 +70,6 @@ class BoxGeometry():
 			[-0.5, 0.5, 0.5],
 			[0.5, 0.5, 0.5],
 		], dtype = 'float32')
-
 		self.vertices *= self.size
 
 		self.normals = numpy.array([
@@ -96,4 +109,36 @@ class BoxGeometry():
 			[0, 1, 0],
 			[0, 1, 0],
 			[0, 1, 0],
+		], dtype = 'float32')
+
+class PlaneGeometry(Geometry):
+	def __init__(
+		self,
+		size = (1, 1, 1),
+		position = (0, 0, 0),
+		rotation = (0, 0, 0),
+		scale = (1, 1, 1)
+	):
+		super().__init__(size, position, rotation, scale)
+
+	def setup_vertex_data(self):
+		half_width = 0.5 * self.size.x
+		half_height = 0.5 * self.size.y
+
+		self.vertices = numpy.array([
+			[-half_width, -half_height, 0.0],
+			[half_width, -half_height, 0.0],
+			[-half_width, half_height, 0.0],
+			[half_width, -half_height, 0.0],
+			[half_width, half_height, 0.0],
+			[-half_width, half_height, 0.0],
+		], dtype = 'float32')
+
+		self.normals = numpy.array([
+			[0, 0, 1],
+			[0, 0, 1],
+			[0, 0, 1],
+			[0, 0, 1],
+			[0, 0, 1],
+			[0, 0, 1],
 		], dtype = 'float32')
--- v2/main.py
+++ v2/main.py
@@ -1,7 +1,7 @@
 from core.renderer import Renderer
 from core.scene import Scene
 from core.camera import Camera
-from core.geometry import BoxGeometry
+from core.geometry import BoxGeometry, PlaneGeometry
 from core.material import SolidMaterial
 from core.mesh import Mesh, MeshInstanced
 
@@ -10,6 +10,8 @@ renderer = Renderer()
 scene = Scene(renderer)
 camera = Camera(renderer)
 
+# BoxGeoemtry Instanced Example
+"""
 def box_update_method(geometry, material, instance_data):
 	geometry.rotation.x += 10 / renderer.delta_time
 	geometry.rotation.y += 10 / renderer.delta_time
@@ -20,12 +22,23 @@ for y in range(50):
 		instance_data.append({ 'position': [x * 1.5 - 37.5, y * 1.5 - 37.5, -90] })
 
 box_mesh = MeshInstanced(
-	geometry = BoxGeometry(size = (1, 1, 1)),
+	geometry = BoxGeometry(),
 	material = SolidMaterial(color = (1, 0.8, 0.7)),
 	shader_program = renderer.shaders['default'],
 	update_method = box_update_method,
 	instance_data = instance_data,
 )
 scene.children.append(box_mesh)
+"""
+
+# PlaneGeometry Example
+"""
+plane_mesh = Mesh(
+	geometry = PlaneGeometry(),
+	material = SolidMaterial(color = (1, 0, 0)),
+	shader_program = renderer.shaders['default'],
+)
+scene.children.append(plane_mesh)
+"""
 
 renderer.run(scene, camera)
